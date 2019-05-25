0b89241faf5cef4d8566d23cd8dd074fd29432e3
ErikJhonatta <erikjhonatta@hotmail.com>
2019-05-25 15:43:47 +0000

    Adicionando pagina inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'escolas#index'
+  root 'welcome#index'
   resources :escolas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
