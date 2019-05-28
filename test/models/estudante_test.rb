864aa810d072fb936b865925ef1fc63a412dccc7
BASDiniz <brunodiniz1818@gmail.com>
2019-05-28 06:51:12 -0300

    Implementando teste de model Nao deve ser possivel salvar um estudante com o cpf em branco - Model: Estudante

--- test/models/estudante_test.rb
+++ test/models/estudante_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class EstudanteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Nao deve ser possivel salvar um estudante com o cpf em branco' do
+    estudante = Estudante.new nome: 'Bruno', cpf: '', serie: '8ª', turma: 'A'
+    assert_not estudante.save
+  end
 end
