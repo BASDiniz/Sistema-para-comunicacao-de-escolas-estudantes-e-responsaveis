6edbc1202b5db6b686b9a1c8966654ad09e7198f
ErikJhonatta <erikjhonatta@hotmail.com>
2019-05-28 02:08:25 +0000

    Adicionando Testes de Model da Classe Escola

--- test/controllers/escolas_controller_test.rb
+++ test/controllers/escolas_controller_test.rb
@@ -15,14 +15,6 @@ class EscolasControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create escola" do
-    assert_difference('Escola.count') do
-      post escolas_url, params: { escola: { cpf_do_administrador: @escola.cpf_do_administrador, nome: @escola.nome, numero_de_estudantes: @escola.numero_de_estudantes, numero_de_professores: @escola.numero_de_professores, numero_de_series: @escola.numero_de_series, numero_de_turmas: @escola.numero_de_turmas } }
-    end
-
-    assert_redirected_to escola_url(Escola.last)
-  end
-
   test "should show escola" do
     get escola_url(@escola)
     assert_response :success
@@ -31,11 +23,6 @@ class EscolasControllerTest < ActionDispatch::IntegrationTest
   test "should get edit" do
     get edit_escola_url(@escola)
     assert_response :success
-  end
-
-  test "should update escola" do
-    patch escola_url(@escola), params: { escola: { cpf_do_administrador: @escola.cpf_do_administrador, nome: @escola.nome, numero_de_estudantes: @escola.numero_de_estudantes, numero_de_professores: @escola.numero_de_professores, numero_de_series: @escola.numero_de_series, numero_de_turmas: @escola.numero_de_turmas } }
-    assert_redirected_to escola_url(@escola)
   end
 
   test "should destroy escola" do
