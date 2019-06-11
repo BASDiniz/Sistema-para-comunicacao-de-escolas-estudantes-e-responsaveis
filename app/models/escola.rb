325b27391de8549e39da872c72078c26c80a0a1f
BASDiniz <brunodiniz1818@gmail.com>
2019-06-11 16:21:36 -0300

    Apagando para fazer os relacionamentos corretamente

--- app/models/escola.rb
+++
@@ -1,8 +0,0 @@
-class Escola < ApplicationRecord
-    validates :nome, :presence => true, :length => {:minimum => 3}, uniqueness: true
-    validates :cpf_do_administrador, presence: true, :length => {:is => 11}, uniqueness: true
-    validates :numero_de_estudantes, :presence => true
-    validates :numero_de_professores, :presence => true
-    validates :numero_de_series, :presence => true
-    validates :numero_de_turmas, :presence => true
-end
