50cf6c1adcd913b6048cd674bb31e074059bd79e
BASDiniz <brunodiniz1818@gmail.com>
2019-06-11 21:59:08 -0300

    Implementação do teste/scenario Visualizar um Estudante cadastrado

--- features/step_definitions/Estudante_stepdefs.rb
+++ features/step_definitions/Estudante_stepdefs.rb
@@ -35,3 +35,39 @@ Then ('Um novo estudante com cpf {string} eh criado') do |cpf|
   expect(page).to have_content(cpf)
   expect(page).to have_current_path(estudantes_path + '/' + Estudante.last.id.to_s)
 end
+
+Given ('Eu estou na pagina de listagem de todos os estudantes') do
+  visit 'estudantes'
+  expect(page).to have_current_path('/estudantes')
+end
+
+When ('Eu clico para visualizar o estudante que possui o cpf {string}') do |cpf|
+  visit root_path
+  visit 'escolas/new'
+  fill_in 'escola_nome', :with => 'UAG'
+  fill_in 'escola_cpfDoDiretor', :with => '70378876414'
+  click_button 'Create Escola'
+  visit root_path
+  visit 'anos/new'
+  select '8ª', :from => "ano[nome]"
+  select "Tarde", :from => "ano[turno]"
+  select "UAG", :from => "ano[escola_id]"
+  click_button 'Create Ano'
+  visit root_path
+  visit 'turmas/new'
+  select 'A', :from => "turma[nome]"
+  select '8ª', :from => "turma[ano_id]"
+  click_button 'Create Turma'
+  visit root_path
+  visit 'estudantes/new'
+  fill_in 'estudante[nome]', :with => 'Bruno'
+  fill_in 'estudante[cpf]', :with => cpf
+  select 'A', :from => 'estudante[turma_id]'
+  click_button 'Create Estudante'
+  click_link 'Back'
+  click_link 'Show'
+end
+
+Then ('Eu vejo o estudante que possui o cpf {string}') do |cpf|
+  expect(page).to have_content(cpf)
+end
