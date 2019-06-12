7a32c1734c56e150ef70ebf9b846c7ee91e2f8a5
BASDiniz <brunodiniz1818@gmail.com>
2019-06-11 22:27:45 -0300

    Implementação do teste de model de Escola - Nao deve ser possivel salvar uma escola com o nome em branco

--- test/models/escola_test.rb
+++ test/models/escola_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class EscolaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Nao deve ser possivel salvar uma escola com nome em branco' do
+    escolaTeste = Escola.new(nome: '',cpfDoDiretor: '12345678910')
+    assert_not escolaTeste.save, "Salvou escola com o nome em branco"
+  end
 end
