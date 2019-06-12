0262e8aad9722fae7b49c1164aa61bee7bf61056
BASDiniz <brunodiniz1818@gmail.com>
2019-06-11 23:01:01 -0300

    Implementação teste de model Ano - Deve ser possivel criar um ano corretamente

--- test/models/ano_test.rb
+++ test/models/ano_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AnoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Deve ser possivel criar um ano corretamente" do
+    escolaTeste = Escola.new(nome:'UAG',cpfDoDiretor: '70378876414')
+    escolaTeste.save
+    anoTeste = Ano.new(nome:'1°',turno:'Tarde',escola: escolaTeste)
+    assert anoTeste.save
+  end
 end
