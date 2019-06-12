f32be34ef8ca70b3321445cd4185f5be9f9dad83
ErikJhonatta <erikjhonatta@hotmail.com>
2019-06-12 01:30:15 +0000

    Adicionando Testes de model de Turma

--- test/models/turma_test.rb
+++ test/models/turma_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class TurmaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Deve ser possivel salvar uma turma corretamente" do
+      escolaTeste = Escola.new(nome:'UAG',cpfDoDiretor: '70378876414')
+      escolaTeste.save
+      anoTeste = Ano.new(nome:'1°',turno:'Tarde',escola: escolaTeste)
+      turmaTeste = Turma.new(nome: 'A',ano: anoTeste)
+      assert turmaTeste.save
+  end
+  test "Nao deve ser possivel salvar uma turma sem nome" do
+      escolaTeste = Escola.new(nome:'UAG',cpfDoDiretor: '70378876414')
+      escolaTeste.save
+      anoTeste = Ano.new(nome:'1°',turno:'Tarde',escola: escolaTeste)
+      turmaTeste = Turma.new(ano: anoTeste)
+      assert_not turmaTeste.save
+  end
+  test "Nao deve ser possivel salvar uma turma sem ano" do
+      escolaTeste = Escola.new(nome:'UAG',cpfDoDiretor: '70378876414')
+      escolaTeste.save
+      anoTeste = Ano.new(nome:'1°',turno:'Tarde',escola: escolaTeste)
+      turmaTeste = Turma.new(nome: 'A')
+      assert_not turmaTeste.save
+  end
 end
