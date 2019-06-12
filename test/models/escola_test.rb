14c2d79a79e26e224f3f1069237dfc7714a0e417
BASDiniz <brunodiniz1818@gmail.com>
2019-06-11 22:30:31 -0300

    Implementação do teste de model Escola - Nao deve ser possivel cadastrar uma escola com cpf do diretor invalido

--- test/models/escola_test.rb
+++ test/models/escola_test.rb
@@ -5,4 +5,9 @@ class EscolaTest < ActiveSupport::TestCase
     escolaTeste = Escola.new(nome: '',cpfDoDiretor: '12345678910')
     assert_not escolaTeste.save, "Salvou escola com o nome em branco"
   end
+
+  test 'Nao deve ser possivel cadastrar uma escola com cpf do diretor invalido' do
+    escolaTeste1 = Escola.new(nome: 'EscolaTeste',cpfDoDiretor: '1234567')
+    assert_not escolaTeste1.save,"Salvou a escola com cpf de adm invalido"
+  end
 end
