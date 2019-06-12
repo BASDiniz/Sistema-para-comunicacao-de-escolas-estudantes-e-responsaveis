88c1472e8811c23be2a000320f61d53f7a8028e3
BASDiniz <brunodiniz1818@gmail.com>
2019-06-11 23:11:34 -0300

    Adicionando código para limpar a base de dados cucumber - Arquivo: env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,3 +4,11 @@ require 'capybara/rspec'
 require 'capybara'
 require 'capybara/cucumber'
 require 'capybara/dsl'
+
+Cucumber::Rails::Database.autorun_database_cleaner = false
+DatabaseCleaner.strategy = :truncation
+Cucumber::Rails::Database.javascript_strategy = :truncation
+
+Before do
+  DatabaseCleaner.clean
+end
