2f3150de45cc706bc0317624111f210748e3bc23
ErikJhonatta <erikjhonatta@hotmail.com>
2019-06-18 15:59:44 +0000

    Resolvendo Issue Codacy no arquivo turmas_controller.rb

--- app/controllers/turmas_controller.rb
+++ app/controllers/turmas_controller.rb
@@ -64,7 +64,7 @@ class TurmasController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_turma
-      @turma = Turma.find(params[:id])
+      @turma = Turma.unscoped.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
